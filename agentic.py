0417eb1c95f02825ad3f754125db8dba21f6bb9b
Tanay <tanayj2107@gmail.com>
2026-01-20 17:22:26 -0500

    added tool decorator using beautifulsoup + response

--- agentic.py
+++ agentic.py
@@ -5,6 +5,8 @@ from langchain_community.agent_toolkits.load_tools import load_tools
 from langgraph.checkpoint.memory import MemorySaver
 from langchain.tools import tool
 from langchain_community.tools import DuckDuckGoSearchRun
+import requests
+from bs4 import BeautifulSoup
 
 load_dotenv()
 
@@ -15,6 +17,16 @@ def web_search(query: str) -> str:
     results = search.run(query)
     return results
 
+@tool
+def fetch_webpage(url: str) -> str:
+    """Fetches and extracts text content from a webpage URL"""
+    response = requests.get(url).text
+    soup = BeautifulSoup(response, "html.parser")
+
+    text = soup.get_text()[:5000]
+    return text
+
+
 # Create memory for the agent
 memory = MemorySaver()
 
@@ -23,7 +35,7 @@ prompt = "What are the latest tech news headlines today?"
 llm = ChatOpenAI(temperature=0, model='gpt-3.5-turbo')
 
 tools = load_tools(['wikipedia', "llm-math"], llm=llm)
-tools.append(web_search)
+tools.extend([web_search, fetch_webpage])
 
 # Create agent with memory using checkpointer parameter
 agent = create_agent(llm, tools, checkpointer=memory, debug=False)
@@ -35,6 +47,8 @@ result = agent.invoke(inputs, config)
 
 final_message = result['messages'][-1]
 print(final_message.content)
+
+    
 
 
 
